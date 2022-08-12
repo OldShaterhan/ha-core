5bec5aeb27d100e8bba2887266dd0b76fd51fefe
Tomasz Wieczorek <tomek12-96@o2.pl>
2022-08-12 20:13:56 +0000

    Sensor

--- homeassistant/components/hafas/sensor.py
+++ homeassistant/components/hafas/sensor.py
@@ -1,6 +1,8 @@
 """Platform for the opengarage.io sensor component."""
 from __future__ import annotations
 
+from datetime import timedelta
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -9,6 +11,9 @@ from ..sensor import SensorEntity
 
 # from .const import DOMAIN
 
+ICON = "mdi:train"
+SCAN_INTERVAL = timedelta(minutes=2)
+
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -16,3 +21,12 @@ async def async_setup_entry(
     """Set up the HAFAS sensor platform."""
 
     async_add_entities([SensorEntity()], True)
+
+
+class HafasSensor(SensorEntity):
+    """Implementation of a HAFAS sensor."""
+
+    @property
+    def icon(self):
+        """Return the icon for the frontend."""
+        return ICON
